2b4642ba0b4e56faeb8c97b0d33d019a158e4abb
Fernando Herrero <alphp@users.noreply.github.com>
2022-04-01 23:00:40 +0200

    Update version to 0.0.6

--- tests/strftimeTest.php
+++ tests/strftimeTest.php
@@ -10,10 +10,18 @@
       $this->int_date = strtotime($this->str_date);
     }
 
-    public function test_ymdhms () {
-        $result = strftime('%Y-%m-%d %H:%M:%S', $this->str_date);
-        $this->assertEquals('2022-03-12 01:02:03', $result);
-        $result = strftime('%Y-%m-%d %H:%M:%S', $this->int_date);
-        $this->assertEquals('2022-03-12 01:02:03', $result);
+    public function test_int_timestamp () {
+      $result = strftime('%Y-%m-%d %H:%M:%S', $this->int_date);
+      $this->assertEquals('2022-03-12 01:02:03', $result);
+    }
+
+    public function test_string_timestamp () {
+      $result = strftime('%Y-%m-%d %H:%M:%S', $this->str_date);
+      $this->assertEquals('2022-03-12 01:02:03', $result);
+    }
+
+    public function test_datetime_timestamp () {
+      $result = strftime('%Y-%m-%d %H:%M:%S', new DateTime($this->str_date));
+      $this->assertEquals('2022-03-12 01:02:03', $result);
     }
   }
