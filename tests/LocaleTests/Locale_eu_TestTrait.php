335e77704661bdf4e5ea9c265f055db3b99723b9
Fernando Herrero <ininflamable@gmail.com>
2024-01-26 01:46:17 +0100

    Fix test

--- tests/LocaleTests/Locale_eu_TestTrait.php
+++ tests/LocaleTests/Locale_eu_TestTrait.php
@@ -20,7 +20,7 @@
       $this->assertEquals('mar.', $result, '%b: Abbreviated month name, based on the locale');
 
       $result = strftime('%B', '20220306 13:02:03', $locale);
-      $this->assertEquals('martxoak', $result, '%B: Full month name, based on the locale');
+      $this->assertMatchesRegularExpression('|martxoak?|', $result, '%B: Full month name, based on the locale');
 
       $result = strftime('%h', '20220306 13:02:03', $locale);
       $this->assertEquals('mar.', $result, '%h: Abbreviated month name, based on the locale (an alias of %b)');
@@ -29,7 +29,7 @@
       $this->assertEquals('13:02:03', $result, '%X: Preferred time representation based on locale, without the date');
 
       $result = strftime('%c', '20220306 13:02:03', $locale);
-      $this->assertEquals('2022(e)ko martxoakren 6(a) 13:02', $result, '%c: Preferred date and time stamp based on locale');
+      $this->assertMatchesRegularExpression('|2022\(e\)ko martxoak?ren 6\(a\) \(?13:02\)?|', $result, '%c: Preferred date and time stamp based on locale');
 
       $result = strftime('%x', '20220306 13:02:03', $locale);
       $this->assertEquals('22/3/6', $result, '%x: Preferred date representation based on locale, without the time');
